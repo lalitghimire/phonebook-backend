06c563de244ba5d89912fe95a996637d8d148bc7
Lalit Ghimire <lalitghimire@gmail.com>
2021-09-28 11:44:58 +0300

    completed 3.7-3.8

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("tiny"));
+morgan.token("postedData", (req) => {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status  :res[content-length] - :response-time ms :postedData"
+  )
+);
 
 // persons
 let persons = [
