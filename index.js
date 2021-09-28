ba7aa4d241708a2f820af680e82408af20fcc4de
Lalit Ghimire <lalitghimire@gmail.com>
2021-09-28 14:57:43 +0300

    added frontend build

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 app.use(morgan("tiny"));
 morgan.token("postedData", (req) => {
   if (req.method === "POST") return JSON.stringify(req.body);
